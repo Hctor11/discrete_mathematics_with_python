daad3b1e390027780498f960e77557f0ee8492d3
hector <hectorrrivera1010@gmail.com>
2023-01-28 20:45:03 -0600

    types and ids

--- files/types.py
+++ files/types.py
@@ -11,3 +11,19 @@ de caracteres. Finalmente, un booleano un tipo de dato binario, esto quiere deci
 solo puede tomar dos valores: True o False.
 '''
 
+'''
+los datos en python son guardados como objetos con su respectivo id en memoria, en este sentido Python
+es de tipado fuerte, aunque el id en memoria puede cambiar con respecto a la ejecucion
+'''
+
+z = 12
+print(id(z))
+
+z = "h"
+print(id(z))
+
+a = "hola"
+b = "H" + a[1:]
+print(b)
+
+print(id(int)) # este id no cambia con respecto al tiempo, ya que es un objeto
