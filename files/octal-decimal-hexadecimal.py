01d202b223e40734c9abd500829fed62ee67e84a
hector <hectorrrivera1010@gmail.com>
2023-01-28 21:20:06 -0600

    git ignore .idea directory

--- files/octal-decimal-hexadecimal.py
+++ files/octal-decimal-hexadecimal.py
@@ -1,2 +1,11 @@
 
-print(int('23', 8))
+print(int('23', 6)) # hexadecimal
+print(int('23', 8)) # octal
+print(int('23'))    # decimal
+
+'''
+cuando ponemos un numero al lado de el objeto que queremos convertir en string,
+este tambien puede adoptar el sistema numero que estemos definiendo al lado
+'''
+
+print(bin(23)) # la funcion bin returna el resultado en formate binario
