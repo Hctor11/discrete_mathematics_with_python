56c8580a24ebafd30c95371488759ddce478d85b
hector <hectorrrivera1010@gmail.com>
2023-01-28 20:36:53 -0600

    tipos de datos

---
+++ files/types.py
@@ -0,0 +1,13 @@
+
+print(type(32))
+print(type(2.3))
+print(type(True))
+print(type("hola"))
+'''
+un tipo entero se refiere a un numero entero ya sea positivo, negativo o cero,
+un float es un una aproximacion de python a los numeros reales, estos consisten de una parte real y
+de una parte decimal. Un String es un tipo de dato en cadena de texto, este consiste en una secuencia
+de caracteres. Finalmente, un booleano un tipo de dato binario, esto quiere decir que un booleano
+solo puede tomar dos valores: True o False.
+'''
+
