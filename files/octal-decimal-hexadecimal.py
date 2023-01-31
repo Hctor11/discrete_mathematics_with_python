7512633ceba7f4819677e96ff40ee54cffc60947
hector <hectorrrivera1010@gmail.com>
2023-01-30 22:11:52 -0600

    commit

--- files/octal-decimal-hexadecimal.py
+++ files/octal-decimal-hexadecimal.py
@@ -9,3 +9,18 @@ este tambien puede adoptar el sistema numero que estemos definiendo al lado
 '''
 
 print(bin(23)) # la funcion bin returna el resultado en formate binario
+
+'''
+a diferencia de otros lenguajes de programacion, en python podemos multiplicar String por 
+la cantidad que querrramos ver en pantalla de ese String
+'''
+
+print("hola" * 100)
+
+'''
+Normalmente las direcciones IPv4 del internet son un numero de 32-bits o de 4-bytes, representados
+en la forma a.b.c.d
+'''
+
+print(float(float('20')))
+print(bool('0.0000001'))
